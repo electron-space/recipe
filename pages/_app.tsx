2f674b0cf833d06960d4c2eadcf6537851850f59
electron <nyhmsaint@gmail.com>
2021-11-28 01:18:04 -0500

    Setting up App functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, { useState, useEffect} from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp() {
+  const [recipeFound, setRecipeFound] = useState([])
+  const [recipeSearch, setRecipeSearch] = useState('')
+
+  const searchForRecipes = async (query:string): Promise<any> => {
+    const result = await fetch(`${query}`)
+    return(await result.json()).results
+  }
+  
+  useEffect(() => {
+    (async () => {
+      const query = encodeURIComponent(recipeSearch)
+      const response = await searchForRecipes(query)
+      setRecipeFound(response)
+   })();
+    
+  })
+  
+
+
+  return (
+    <div className='App'>
+      <h1> Recipe Search</h1>
+      <form className='searchForm'>
+        <input id='searchText' type="text" />
+        <button className=''>Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default MyApp
